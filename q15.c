b3183a4c8b6c459dca1e0fc71ee8cb1e874b0bb8
sandeep889 <49287980+sandeep889@users.noreply.github.com>
2019-04-17 22:22:12 +0530

    Update q15.c

--- q15.c
+++ q15.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<conio.h>
-int main()
+int main()     
 {
     int B_T[10],Process_no[10],W_T[10],T_A_T[10];
     int A_T[10],Prior[10],i,j,Number_of_Process,tot=0,flag,temp,Avg_W_T,Avg_T_A_T;
